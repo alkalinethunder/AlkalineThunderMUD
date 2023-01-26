1a2291827b416c1a53895647ee8fff5c4aea6a75
Jet the Hawk <jet@the-babylon-garden.local.acidiclight.dev>
2023-01-26 00:56:26 -0500

    Make something that vaguely resembles a shell

--- src/AlkalineThunderMud.Shell/Program.cs
+++ src/AlkalineThunderMud.Shell/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("You have connected to the Socially Distant MUD successfully.");
+
+bool running = true;
+
+Console.WriteLine("Type 'exit' to disconnect. This prompt will echo whatever you type.");
+
+while (running)
+{
+    Console.Write("> ");
+    string line = Console.ReadLine();
+    if (line == "exit")
+        running = false;
+
+    Console.WriteLine(line);
+}
